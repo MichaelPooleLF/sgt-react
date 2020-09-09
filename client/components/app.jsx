a8713a4ada5b50ba894ad04a85edd3c4d080d002
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-09-09 14:45:07 -0500

    added grade and gradetable components

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -19,7 +19,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <GradeTable grades={this.state.grades} />
+      </>
     );
   }
 }
@@ -30,6 +33,38 @@ class Header extends React.Component {
       <header>
         <h1>Student Grade Table</h1>
       </header>
+    );
+  }
+}
+
+class Grade extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.grade.name}</td>
+        <td>{this.props.grade.course}</td>
+        <td>{this.props.grade.grade}</td>
+      </tr>
+    );
+  }
+}
+
+class GradeTable extends React.Component {
+  render() {
+    const tableData = this.props.grades.map(element => {
+      return <Grade key={element.id} grade={element} />;
+    });
+    return (
+      <table className="table table-striped">
+        <thead className="thead-light">
+          <th>Student Name</th>
+          <th>Course</th>
+          <th>Grade</th>
+        </thead>
+        <tbody>
+          {tableData}
+        </tbody>
+      </table>
     );
   }
 }
